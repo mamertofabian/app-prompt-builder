0a54c2af3f94118218256adbdbed2134a828e4a1
Mamerto Fabian Jr <mfabianjr@gmail.com>
2024-11-06 15:14:13 +0800

    feat: Migrate project from React to Svelte with configuration updates

--- svelte.config.js
+++ svelte.config.js
@@ -10,3 +10,15 @@ const config = {
 };
 
 export default config;
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: vitePreprocess()
+};
+
+export default config;
